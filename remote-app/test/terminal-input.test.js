f6c226acf82c3aaa15773bccaf7ee52295f7a1e4
chefdique <richardfadair@gmail.com>
2026-05-19 22:59:31 -0700

    Revert "fix(agentremote): send control word shortcuts as tmux keys"
    This reverts commit ee23c13e83954bd9a07e5833db244bd0e86b1811.

--- remote-app/test/terminal-input.test.js
+++ remote-app/test/terminal-input.test.js
@@ -28,17 +28,6 @@ test('terminal input maps Option/Alt word navigation and deletion to readline by
   assert.equal(terminalWordShortcutBytes(keydown({ key: 'd', code: 'KeyD' })), '\x1bd');
 });
 
-test('terminal input maps Control word-edit shortcuts to real control bytes', () => {
-  assert.equal(terminalWordShortcutBytes(keydown({ altKey: false, ctrlKey: true, key: 'ArrowLeft', code: 'ArrowLeft' })), '\x1bb');
-  assert.equal(terminalWordShortcutBytes(keydown({ altKey: false, ctrlKey: true, key: 'ArrowRight', code: 'ArrowRight' })), '\x1bf');
-  assert.equal(terminalWordShortcutBytes(keydown({ altKey: false, ctrlKey: true, key: 'Backspace', code: 'Backspace' })), '\x17');
-  assert.equal(terminalWordShortcutBytes(keydown({ altKey: false, ctrlKey: true, key: 'Delete', code: 'Delete' })), '\x1bd');
-  assert.equal(terminalWordShortcutBytes(keydown({ altKey: false, ctrlKey: true, key: 'w', code: 'KeyW' })), '\x17');
-  assert.equal(terminalWordShortcutBytes(keydown({ altKey: false, ctrlKey: true, key: 'u', code: 'KeyU' })), '\x15');
-  assert.equal(terminalWordShortcutBytes(keydown({ altKey: false, ctrlKey: true, key: 'k', code: 'KeyK' })), '\x0b');
-  assert.equal(terminalWordShortcutBytes(keydown({ altKey: false, ctrlKey: true, key: 'c', code: 'KeyC' })), '\x03');
-});
-
 test('terminal input leaves paste and non-Alt shortcuts alone', () => {
   assert.equal(terminalWordShortcutBytes(keydown({ altKey: false, ctrlKey: true, key: 'v', code: 'KeyV' })), null);
   assert.equal(terminalWordShortcutBytes(keydown({ altKey: true, metaKey: true, key: 'ArrowLeft', code: 'ArrowLeft' })), null);
@@ -46,15 +35,6 @@ test('terminal input leaves paste and non-Alt shortcuts alone', () => {
 });
 
 test('terminal input converts readline and xterm modified-key bytes to tmux keys', () => {
-  assert.deepEqual(tmuxKeysForTerminalInput('\x01'), ['C-a']);
-  assert.deepEqual(tmuxKeysForTerminalInput('\x03'), ['C-c']);
-  assert.deepEqual(tmuxKeysForTerminalInput('\x04'), ['C-d']);
-  assert.deepEqual(tmuxKeysForTerminalInput('\x05'), ['C-e']);
-  assert.deepEqual(tmuxKeysForTerminalInput('\x0b'), ['C-k']);
-  assert.deepEqual(tmuxKeysForTerminalInput('\x15'), ['C-u']);
-  assert.deepEqual(tmuxKeysForTerminalInput('\x17'), ['C-w']);
-  assert.deepEqual(tmuxKeysForTerminalInput('\x7f'), ['BSpace']);
-  assert.deepEqual(tmuxKeysForTerminalInput('\b'), ['BSpace']);
   assert.deepEqual(tmuxKeysForTerminalInput('\x1bb'), ['Escape', 'b']);
   assert.deepEqual(tmuxKeysForTerminalInput('\x1bf'), ['Escape', 'f']);
   assert.deepEqual(tmuxKeysForTerminalInput('\x1b\x7f'), ['Escape', 'BSpace']);
@@ -63,10 +43,5 @@ test('terminal input converts readline and xterm modified-key bytes to tmux keys
   assert.deepEqual(tmuxKeysForTerminalInput('\x1b[1;3D'), ['Escape', 'b']);
   assert.deepEqual(tmuxKeysForTerminalInput('\x1b[1;3C'), ['Escape', 'f']);
   assert.deepEqual(tmuxKeysForTerminalInput('\x1b[3;3~'), ['Escape', 'd']);
-  assert.deepEqual(tmuxKeysForTerminalInput('\x1b[1;5D'), ['Escape', 'b']);
-  assert.deepEqual(tmuxKeysForTerminalInput('\x1b[1;5C'), ['Escape', 'f']);
-  assert.deepEqual(tmuxKeysForTerminalInput('\x1b[3;5~'), ['Escape', 'd']);
-  assert.deepEqual(tmuxKeysForTerminalInput('\x1b[127;5u'), ['C-w']);
-  assert.deepEqual(tmuxKeysForTerminalInput('\x1b[8;5u'), ['C-w']);
   assert.equal(tmuxKeysForTerminalInput('plain text'), null);
 });
